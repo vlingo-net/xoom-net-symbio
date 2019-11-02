ba0f160f3fd6518376ab425d1a68cf24bc072a08
Tomasz Jaskula <thomasv1000@hotmail.fr>
2019-11-02 15:40:52 +0100

    Implement State interfaces

---
+++ src/Vlingo.Symbio/Store/State/IStateStoreEntryReader.cs
@@ -0,0 +1,21 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+namespace Vlingo.Symbio.Store.State
+{
+    /// <summary>
+    /// The reader for a given <see cref="IStateStore"/>, which is provided by its w <code>EntryReader()</code> method.
+    /// The <see cref="IEntry{T}"/> instances are appended by the <see cref="IStateStore"/> <code>Write(...)</code> methods.
+    /// This reads sequentially over all <see cref="IEntry{T}"/> instances in the entire storage, from the
+    /// first written <see cref="IEntry{T}"/> to the current last written <see cref="IEntry{T}"/>, and is prepared to read
+    /// all newly appended <see cref="IEntry{T}"/> instances beyond that point when they become available.
+    /// </summary>
+    /// <typeparam name="TEntry">the concrete type of <see cref="IEntry{T}"/> stored and read, which maybe be string, byte[], or object</typeparam>
+    public interface IStateStoreEntryReader<TEntry>
+    {
+    }
+}
---
+++ src/Vlingo.Symbio/Store/State/IWriteResultInterest.cs
@@ -0,0 +1,29 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System.Collections.Generic;
+using Vlingo.Common;
+
+namespace Vlingo.Symbio.Store.State
+{
+    public interface IWriteResultInterest
+    {
+        /// <summary>
+        /// Implemented by the interest of a given State Store for write operation results.
+        /// </summary>
+        /// <param name="outcome">The <see cref="IOutcome{TFailure, TSuccess}"/> of the write</param>
+        /// <param name="id">The string unique identity of the state to attempted write</param>
+        /// <param name="state">the <typeparamref name="TState"/> native state that was possibly written</param>
+        /// <param name="stateVersion">The int version of the state that was possibly written</param>
+        /// <param name="sources">The <code>IEnumerable{Source{TSource}}</code> if any</param>
+        /// <param name="object">The Object passed to write() that is sent back to the receiver</param>
+        /// <typeparam name="TState">The native state type</typeparam>
+        /// <typeparam name="TSource">The native source type</typeparam>
+        void WriteResultedIn<TState, TSource>(IOutcome<StorageException, Result> outcome, string id, TState state,
+            int stateVersion, IEnumerable<Source<TSource>> sources, object @object);
+    }
+}
