1f6839cd797d6c267f5960186050ed44e306d01d
Tomasz Jaskula <thomasv1000@hotmail.fr>
2019-11-07 18:09:58 +0100

    WIP StateSotreEntreReader tests

--- src/Vlingo.Symbio.Tests/Store/State/InMemory/InMemoryStateStoreEntryReaderActorTest.cs
+++ src/Vlingo.Symbio.Tests/Store/State/InMemory/InMemoryStateStoreEntryReaderActorTest.cs
@@ -6,6 +6,7 @@
 // one at https://mozilla.org/MPL/2.0/.
 
 using System;
+using System.Collections.Generic;
 using Vlingo.Actors;
 using Vlingo.Actors.TestKit;
 using Vlingo.Symbio.Store.State;
@@ -17,10 +18,10 @@ namespace Vlingo.Symbio.Tests.Store.State.InMemory
 {
     public class InMemoryStateStoreEntryReaderActorTest
     {
-        private static string _id1 = "123-A";
-        private static string _id2 = "123-B";
-        private static string _id3 = "123-C";
-        
+        private const string Id1 = "123-A";
+        private const string Id2 = "123-B";
+        private const string Id3 = "123-C";
+
         private readonly MockStateStoreDispatcher<TextEntry, string> _dispatcher;
         private readonly EntryAdapterProvider _entryAdapterProvider;
         private readonly MockStateStoreResultInterest _interest;
@@ -28,10 +29,29 @@ namespace Vlingo.Symbio.Tests.Store.State.InMemory
         private readonly IStateStore<TextEntry> _store;
         private readonly World _world;
 
-        [Fact]
+        [Fact(Skip = "Fails")]
         public void TestThatEntryReaderReadsOne()
         {
-            
+            var access = _interest.AfterCompleting<Entity1, TextEntry>(3);
+            _dispatcher.AfterCompleting(0);
+
+            _store.Write(Id1, new Entity1(Id1, 10), 1, new List<Source<Event>> {new Event1()}, _interest);
+            _store.Write(Id2, new Entity2(Id2, "20"), 1, new List<Source<Event>> {new Event2()}, _interest);
+            _store.Write(Id3, new Entity1(Id3, 30), 1, new List<Source<Event>> {new Event3()}, _interest);
+
+            Assert.Equal(new Event1(), access.ReadFrom<Event1>("sources"));
+            Assert.Equal(new Event2(), access.ReadFrom<Event2>("sources"));
+            Assert.Equal(new Event3(), access.ReadFrom<Event3>("sources"));
+
+            var entry1 = _reader.ReadNext().Await();
+            Assert.Equal(_entryAdapterProvider.AsEntry<Event, TextEntry>(new Event1(), Metadata.NullMetadata(), new DefaultTextEntryAdapter<Event>()).WithId("0"), entry1);
+            var entry2 = _reader.ReadNext().Await();
+            Assert.Equal(_entryAdapterProvider.AsEntry<Event, TextEntry>(new Event2(), Metadata.NullMetadata(), new DefaultTextEntryAdapter<Event>()).WithId("1"), entry2);
+            var entry3 = _reader.ReadNext().Await();
+            Assert.Equal(_entryAdapterProvider.AsEntry<Event, TextEntry>(new Event3(), Metadata.NullMetadata(), new DefaultTextEntryAdapter<Event>()).WithId("2"), entry3);
+
+            _reader.Rewind();
+            Assert.Equal(new List<IEntry<TextEntry>> { entry1, entry2, entry3}, _reader.ReadNext(3).Await());
         }
 
         public InMemoryStateStoreEntryReaderActorTest(ITestOutputHelper output)
@@ -53,7 +73,6 @@ namespace Vlingo.Symbio.Tests.Store.State.InMemory
 
             _store = _world.ActorFor<IStateStore<TextEntry>>(typeof(InMemoryStateStoreActor<string, TextEntry>), _dispatcher);
             
-            // TODO: here we have the wrong returned type
             var completes = _store.EntryReader("test");
             _reader = completes.Await<IStateStoreEntryReader<TextEntry>>();
 
@@ -62,7 +81,7 @@ namespace Vlingo.Symbio.Tests.Store.State.InMemory
         }
     }
     
-    public class Event : Source<Event>
+    public abstract class Event : Source<Event>
     {
         public override bool Equals(object obj)
         {
@@ -73,5 +92,17 @@ namespace Vlingo.Symbio.Tests.Store.State.InMemory
             
             return true;
         }
+    }
+
+    public class Event1 : Event
+    {
+    }
+    
+    public class Event2 : Event
+    {
+    }
+    
+    public class Event3 : Event
+    {
     }
 }
