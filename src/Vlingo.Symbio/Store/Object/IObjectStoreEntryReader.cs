d19edc2eb4b6504e95dce374912ad8bc35e26de3
Tomasz Jaskula <thomasv1000@hotmail.fr>
2019-11-29 14:19:14 +0100

    Implement IObjectStoreEntryReader

---
+++ src/Vlingo.Symbio/Store/Object/IObjectStoreEntryReader.cs
@@ -0,0 +1,22 @@
+// Copyright © 2012-2020 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+namespace Vlingo.Symbio.Store.Object
+{
+    /// <summary>
+    /// The reader for a given <see cref="IObjectStore"/>, which is provided by its w <code>EntryReader()</code> method.
+    /// The <see cref="IEntry{T}"/> instances are appended by the <see cref="IObjectStore"/> <code>Persist(...)</code> methods.
+    /// This reads sequentially over all <see cref="IEntry{T}"/> instances in the entire storage, from the
+    /// first written <see cref="IEntry{T}"/> to the current last written <see cref="IEntry{T}"/>, and is prepared to read
+    /// all newly appended <see cref="IEntry{T}"/> instances beyond that point when they become available.
+    /// </summary>
+    /// <typeparam name="TEntry">the concrete type of <see cref="IEntry{T}"/> stored and read, which maybe be string, byte[], or object</typeparam>
+    public interface IObjectStoreEntryReader<TEntry> : IEntryReader<TEntry>
+    {
+        
+    }
+}
