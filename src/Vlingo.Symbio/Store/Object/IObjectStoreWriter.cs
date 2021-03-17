04bf437192f990c1114b2ef8121f4d799f688edc
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-03-18 00:40:17 +0100

    Transform Source class to ISource interface

--- src/Vlingo.Symbio/Store/Object/IObjectStoreWriter.cs
+++ src/Vlingo.Symbio/Store/Object/IObjectStoreWriter.cs
@@ -19,7 +19,7 @@ namespace Vlingo.Symbio.Store.Object
         /// </summary>
         /// <param name="stateSources">The Object to persist with the domain events that were its source of truth</param>
         /// <param name="interest">The <see cref="IPersistResultInterest"/> to which the result is dispatched</param>
-        void Persist<TState, TSource>(StateSources<TState, TSource> stateSources, IPersistResultInterest interest) where TState : StateObject where TSource : Source;
+        void Persist<TState, TSource>(StateSources<TState, TSource> stateSources, IPersistResultInterest interest) where TState : StateObject where TSource : ISource;
         
         /// <summary>
         /// Persists the new <paramref name="stateSources"/> with sources and <see cref="Metadata"/>.
@@ -27,7 +27,7 @@ namespace Vlingo.Symbio.Store.Object
         /// <param name="stateSources">The Object to persist with the domain events that were its source of truth</param>
         /// <param name="metadata">The <see cref="Metadata"/> associated with the <paramref name="stateSources"/> and sources</param>
         /// <param name="interest">The <see cref="IPersistResultInterest"/> to which the result is dispatched</param>
-        void Persist<TState, TSource>(StateSources<TState, TSource> stateSources, Metadata metadata, IPersistResultInterest interest) where TState : StateObject where TSource : Source;
+        void Persist<TState, TSource>(StateSources<TState, TSource> stateSources, Metadata metadata, IPersistResultInterest interest) where TState : StateObject where TSource : ISource;
         
         /// <summary>
         /// Persists the new <paramref name="stateSources"/> with sources.
@@ -35,7 +35,7 @@ namespace Vlingo.Symbio.Store.Object
         /// <param name="stateSources">The Object to persist with the domain events that were its source of truth</param>
         /// <param name="interest">The <see cref="IPersistResultInterest"/> to which the result is dispatched</param>
         /// <param name="object">An object sent to the <see cref="IPersistResultInterest"/> when the persist has succeeded or failed</param>
-        void Persist<TState, TSource>(StateSources<TState, TSource> stateSources, IPersistResultInterest interest, object? @object) where TState : StateObject where TSource : Source;
+        void Persist<TState, TSource>(StateSources<TState, TSource> stateSources, IPersistResultInterest interest, object? @object) where TState : StateObject where TSource : ISource;
         
         /// <summary>
         /// Persists the new <paramref name="stateSources"/> with sources and <see cref="Metadata"/>.
@@ -44,7 +44,7 @@ namespace Vlingo.Symbio.Store.Object
         /// <param name="metadata">The <see cref="Metadata"/> associated with the <paramref name="stateSources"/> and sources</param>
         /// <param name="interest">The <see cref="IPersistResultInterest"/> to which the result is dispatched</param>
         /// <param name="object">An object sent to the <see cref="IPersistResultInterest"/> when the persist has succeeded or failed</param>
-        void Persist<TState, TSource>(StateSources<TState, TSource> stateSources, Metadata metadata, IPersistResultInterest interest, object? @object) where TState : StateObject where TSource : Source;
+        void Persist<TState, TSource>(StateSources<TState, TSource> stateSources, Metadata metadata, IPersistResultInterest interest, object? @object) where TState : StateObject where TSource : ISource;
         
         /// <summary>
         /// Persists the new <paramref name="stateSources"/> with sources as new or updated depending on the value of <paramref name="updateId"/>.
@@ -52,7 +52,7 @@ namespace Vlingo.Symbio.Store.Object
         /// <param name="stateSources">The Object to persist with the domain events that were its source of truth</param>
         /// <param name="updateId">The long identity to facilitate update; &lt; 0 for create &gt; 0 for update</param>
         /// <param name="interest">The <see cref="IPersistResultInterest"/> to which the result is dispatched</param>
-        void Persist<TState, TSource>(StateSources<TState, TSource> stateSources, long updateId, IPersistResultInterest interest) where TState : StateObject where TSource : Source;
+        void Persist<TState, TSource>(StateSources<TState, TSource> stateSources, long updateId, IPersistResultInterest interest) where TState : StateObject where TSource : ISource;
         
         /// <summary>
         /// Persists the new <paramref name="stateSources"/> with sources and <see cref="Metadata"/>.
@@ -61,7 +61,7 @@ namespace Vlingo.Symbio.Store.Object
         /// <param name="metadata">The <see cref="Metadata"/> associated with the <paramref name="stateSources"/> and sources</param>
         /// <param name="updateId">The long identity to facilitate update; &lt; 0 for create &gt; 0 for update</param>
         /// <param name="interest">The <see cref="IPersistResultInterest"/> to which the result is dispatched</param>
-        void Persist<TState, TSource>(StateSources<TState, TSource> stateSources, Metadata metadata, long updateId, IPersistResultInterest interest) where TState : StateObject where TSource : Source;
+        void Persist<TState, TSource>(StateSources<TState, TSource> stateSources, Metadata metadata, long updateId, IPersistResultInterest interest) where TState : StateObject where TSource : ISource;
         
         /// <summary>
         /// Persists the new <paramref name="stateSources"/> with sources as new or updated depending on the value of <paramref name="updateId"/>.
@@ -70,7 +70,7 @@ namespace Vlingo.Symbio.Store.Object
         /// <param name="updateId">The long identity to facilitate update; &lt; 0 for create &gt; 0 for update</param>
         /// <param name="interest">The <see cref="IPersistResultInterest"/> to which the result is dispatched</param>
         /// <param name="object">An object sent to the <see cref="IPersistResultInterest"/> when the persist has succeeded or failed</param>
-        void Persist<TState, TSource>(StateSources<TState, TSource> stateSources, long updateId, IPersistResultInterest interest, object? @object) where TState : StateObject where TSource : Source;
+        void Persist<TState, TSource>(StateSources<TState, TSource> stateSources, long updateId, IPersistResultInterest interest, object? @object) where TState : StateObject where TSource : ISource;
         
         /// <summary>
         /// Persists the new <paramref name="stateSources"/> with sources and <see cref="Metadata"/>.
@@ -80,14 +80,14 @@ namespace Vlingo.Symbio.Store.Object
         /// <param name="updateId">The long identity to facilitate update; &lt; 0 for create &gt; 0 for update</param>
         /// <param name="interest">The <see cref="IPersistResultInterest"/> to which the result is dispatched</param>
         /// <param name="object">An object sent to the <see cref="IPersistResultInterest"/> when the persist has succeeded or failed</param>
-        void Persist<TState, TSource>(StateSources<TState, TSource> stateSources, Metadata metadata, long updateId, IPersistResultInterest interest, object? @object) where TState : StateObject where TSource : Source;
+        void Persist<TState, TSource>(StateSources<TState, TSource> stateSources, Metadata metadata, long updateId, IPersistResultInterest interest, object? @object) where TState : StateObject where TSource : ISource;
 
         /// <summary>
         /// Persists the new <paramref name="allStateSources"/> with sources.
         /// </summary>
         /// <param name="allStateSources">The Objects to persist, each with the domain events that were its source of truth</param>
         /// <param name="interest">The <see cref="IPersistResultInterest"/> to which the result is dispatched</param>
-        void PersistAll<TState, TSource>(IEnumerable<StateSources<TState, TSource>> allStateSources, IPersistResultInterest interest) where TState : StateObject where TSource : Source;
+        void PersistAll<TState, TSource>(IEnumerable<StateSources<TState, TSource>> allStateSources, IPersistResultInterest interest) where TState : StateObject where TSource : ISource;
 
         /// <summary>
         /// Persists the new <paramref name="allStateSources"/> with sources and <see cref="Metadata"/>.
@@ -95,7 +95,7 @@ namespace Vlingo.Symbio.Store.Object
         /// <param name="allStateSources">The Objects to persist, each with the domain events that were its source of truth</param>
         /// <param name="metadata">The <see cref="Metadata"/> associated with the <paramref name="allStateSources"/> and sources</param>
         /// <param name="interest">The <see cref="IPersistResultInterest"/> to which the result is dispatched</param>
-        void PersistAll<TState, TSource>(IEnumerable<StateSources<TState, TSource>> allStateSources, Metadata metadata, IPersistResultInterest interest) where TState : StateObject where TSource : Source;
+        void PersistAll<TState, TSource>(IEnumerable<StateSources<TState, TSource>> allStateSources, Metadata metadata, IPersistResultInterest interest) where TState : StateObject where TSource : ISource;
 
         /// <summary>
         /// Persists the new <paramref name="allStateSources"/> with sources.
@@ -103,7 +103,7 @@ namespace Vlingo.Symbio.Store.Object
         /// <param name="allStateSources">The Objects to persist, each with the domain events that were its source of truth</param>
         /// <param name="interest">The <see cref="IPersistResultInterest"/> to which the result is dispatched</param>
         /// <param name="object">An object sent to the <see cref="IPersistResultInterest"/> when the persist has succeeded or failed</param>
-        void PersistAll<TState, TSource>(IEnumerable<StateSources<TState, TSource>> allStateSources, IPersistResultInterest interest, object? @object) where TState : StateObject where TSource : Source;
+        void PersistAll<TState, TSource>(IEnumerable<StateSources<TState, TSource>> allStateSources, IPersistResultInterest interest, object? @object) where TState : StateObject where TSource : ISource;
         
         /// <summary>
         /// Persists the new <paramref name="allStateSources"/> with sources and <see cref="Metadata"/>.
@@ -112,7 +112,7 @@ namespace Vlingo.Symbio.Store.Object
         /// <param name="metadata">The <see cref="Metadata"/> associated with the <paramref name="allStateSources"/> and sources</param>
         /// <param name="interest">The <see cref="IPersistResultInterest"/> to which the result is dispatched</param>
         /// <param name="object">An object sent to the <see cref="IPersistResultInterest"/> when the persist has succeeded or failed</param>
-        void PersistAll<TState, TSource>(IEnumerable<StateSources<TState, TSource>> allStateSources, Metadata metadata, IPersistResultInterest interest, object? @object) where TState : StateObject where TSource : Source;
+        void PersistAll<TState, TSource>(IEnumerable<StateSources<TState, TSource>> allStateSources, Metadata metadata, IPersistResultInterest interest, object? @object) where TState : StateObject where TSource : ISource;
         
         /// <summary>
         /// Persists the new <paramref name="allStateSources"/> with sources as new or updated depending on the value of <paramref name="updateId"/>.
@@ -120,7 +120,7 @@ namespace Vlingo.Symbio.Store.Object
         /// <param name="allStateSources">The Objects to persist, each with the domain events that were its source of truth</param>
         /// <param name="updateId">The long identity to facilitate update; &lt; 0 for create &gt; 0 for update</param>
         /// <param name="interest">The <see cref="IPersistResultInterest"/> to which the result is dispatched</param>
-        void PersistAll<TState, TSource>(IEnumerable<StateSources<TState, TSource>> allStateSources, long updateId, IPersistResultInterest interest) where TState : StateObject where TSource : Source;
+        void PersistAll<TState, TSource>(IEnumerable<StateSources<TState, TSource>> allStateSources, long updateId, IPersistResultInterest interest) where TState : StateObject where TSource : ISource;
         
         /// <summary>
         /// Persists the new <paramref name="allStateSources"/> with sources and <see cref="Metadata"/>.
@@ -129,7 +129,7 @@ namespace Vlingo.Symbio.Store.Object
         /// <param name="metadata">The <see cref="Metadata"/> associated with the <paramref name="allStateSources"/> and sources</param>
         /// <param name="updateId">The long identity to facilitate update; &lt; 0 for create &gt; 0 for update</param>
         /// <param name="interest">The <see cref="IPersistResultInterest"/> to which the result is dispatched</param>
-        void PersistAll<TState, TSource>(IEnumerable<StateSources<TState, TSource>> allStateSources, Metadata metadata, long updateId, IPersistResultInterest interest) where TState : StateObject where TSource : Source;
+        void PersistAll<TState, TSource>(IEnumerable<StateSources<TState, TSource>> allStateSources, Metadata metadata, long updateId, IPersistResultInterest interest) where TState : StateObject where TSource : ISource;
                
         /// <summary>
         /// Persists the new <paramref name="allStateSources"/> with sources as new or updated depending on the value of <paramref name="updateId"/>.
@@ -138,7 +138,7 @@ namespace Vlingo.Symbio.Store.Object
         /// <param name="updateId">The long identity to facilitate update; &lt; 0 for create &gt; 0 for update</param>
         /// <param name="interest">The <see cref="IPersistResultInterest"/> to which the result is dispatched</param>
         /// <param name="object">An object sent to the <see cref="IPersistResultInterest"/> when the persist has succeeded or failed</param>
-        void PersistAll<TState, TSource>(IEnumerable<StateSources<TState, TSource>> allStateSources, long updateId, IPersistResultInterest interest, object? @object) where TState : StateObject where TSource : Source;
+        void PersistAll<TState, TSource>(IEnumerable<StateSources<TState, TSource>> allStateSources, long updateId, IPersistResultInterest interest, object? @object) where TState : StateObject where TSource : ISource;
         
         /// <summary>
         /// Persists the new <paramref name="allStateSources"/> with sources and <see cref="Metadata"/>.
@@ -148,6 +148,6 @@ namespace Vlingo.Symbio.Store.Object
         /// <param name="updateId">The long identity to facilitate update; &lt; 0 for create &gt; 0 for update</param>
         /// <param name="interest">The <see cref="IPersistResultInterest"/> to which the result is dispatched</param>
         /// <param name="object">An object sent to the <see cref="IPersistResultInterest"/> when the persist has succeeded or failed</param>
-        void PersistAll<TState, TSource>(IEnumerable<StateSources<TState, TSource>> allStateSources, Metadata metadata, long updateId, IPersistResultInterest interest, object? @object) where TState : StateObject where TSource : Source;
+        void PersistAll<TState, TSource>(IEnumerable<StateSources<TState, TSource>> allStateSources, Metadata metadata, long updateId, IPersistResultInterest interest, object? @object) where TState : StateObject where TSource : ISource;
     }
 }
