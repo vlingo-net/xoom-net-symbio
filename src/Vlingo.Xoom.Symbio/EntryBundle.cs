3e68914fb91c19a4cf61a040a7e9fb6ab31040ce
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-08-21 15:51:34 +0200

    Implement EntryBundle

---
+++ src/Vlingo.Xoom.Symbio/EntryBundle.cs
@@ -0,0 +1,31 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+namespace Vlingo.Xoom.Symbio
+{
+    public class EntryBundle
+    {
+        public IEntry Entry { get; }
+        public ISource? Source { get; }
+
+        public EntryBundle(IEntry entry, ISource source)
+        {
+            Entry = entry;
+            Source = source;
+        }
+
+        public EntryBundle(IEntry entry)
+        {
+            Entry = entry;
+            Source = null;
+        }
+
+        public IEntry<TEntry> TypedEntry<TEntry>() => (IEntry<TEntry>) Entry;
+
+        public Source<TSource>? TypedSource<TSource>() => Source as Source<TSource>;
+    }
+}
